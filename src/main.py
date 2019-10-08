34c9dceabf5dc30eee09d6e89995ac574438842d
bernharl <bernharl@student.matnat.uio.no>
2019-10-08 11:54:03 +0200

    Added preprocessing method to main class

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.preprocessing as sklpre
 
-class LogisticRegression():
-    def __init__():
-        pass 
+
+class RegressionClass:
+    def __init__(self):
+        pass
 
     @staticmethod
-    def preprocessing(X, y):
-        pass 
+    def preprocessing(X, y, test_ratio=0.33):
+        X = sklpre.scale(X, copy=False)
+        X_train, X_test, y_train, y_test = sklpre.train_test_split(X, y, test_ratio)
+        return X_train, X_test, y_train, y_test
+
+
+if __name__ == "__main__":
+    pass
