3e046a89079800eb9f13b88dd3584399d4d008b7
Lise Nguyen <lise_nguyen@outlook.com>
2019-10-08 15:22:06 +0200

    bugfixes, tried adding gradient descent method

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.preprocessing as sklpre
-
+import sklearn.model_selection as sklms
 
 class RegressionClass:
     def __init__(self, learning_rate=0.1, batches=1, penalty=None):
@@ -12,8 +12,10 @@ class RegressionClass:
     def fit(self, X=None, y=None):
         raise RuntimeError("Please do not use this class directly.")
 
-    def gradient_descent(self, beta, X, y):
-        pass
+    def gradient_descent(self, beta, X, y, N_iterations=1000):
+        for i in range(N_iterations):
+            beta = -self.learning_rate * self.grad_cost_function(beta, X, y)
+        
 
     def accuracy_score(self, X, y):
         return np.mean(self.predict(X) == y)
@@ -21,17 +23,18 @@ class RegressionClass:
     @staticmethod
     def preprocessing(X, y, test_ratio=0.33):
         X = sklpre.scale(X, copy=False)
-        X_train, X_test, y_train, y_test = sklpre.train_test_split(X, y, test_ratio)
+        X_train, X_test, y_train, y_test = sklms.train_test_split(X, y, test_size=test_ratio)
         return X_train, X_test, y_train, y_test
 
 
 class LogisticRegression(RegressionClass):
     def fit(self, X, y):
-        beta = np.random.normal(0, 1, size=X.shape[0])
+        beta = np.random.normal(0, 1, size=X.shape[1])
         self.gradient_descent(beta, X, y)
+        self.beta = beta 
 
     def p(self, beta, X):
-        exp_expression = np.exp(X.T @ beta)
+        exp_expression = np.exp(X @ beta)
         return exp_expression / (1 + exp_expression)
 
     def grad_cost_function(self, beta, X, y):
