e96f574ef7a7721b87e5d54eb762aa3fff901dff
Lise Nguyen <lise_nguyen@outlook.com>
2019-10-14 08:26:22 +0200

    now scales the data correctly

--- src/read_credit_data.py
+++ src/read_credit_data.py
@@ -4,39 +4,49 @@ import pandas as pd
 import sklearn.preprocessing as sklpre
 import sklearn.compose as sklco
 
+
 # Retrieve credit card data
 cwd = os.getcwd()
 filename = cwd + "/data/default of credit card clients.xls"
 df = pd.read_excel(filename, header=1)
+
+
 
 # Define design matrix X and targets y
 features = df.loc[:, (df.columns != "default payment next month") & (df.columns != "ID")]
 X = features.values
 y = df["default payment next month"].values
 
+
 # Removing feature outliers
+# Marriage
 y = y[np.logical_and(X[:, 3] >= 1, X[:, 3] <= 3)]
 X = X[np.logical_and(X[:, 3] >= 1, X[:, 3] <= 3)]
 
+# Education
 y = y[np.logical_and(X[:, 2] >= 1, X[:, 2] <= 4)]
 X = X[np.logical_and(X[:, 2] >= 1, X[:, 2] <= 4)]
 
+# Sex
 y = y[np.logical_and(X[:, 1] >= 1, X[:, 1] <= 2)]
 X = X[np.logical_and(X[:, 1] >= 1, X[:, 1] <= 2)]
 
-"""
-for i in range(5, 11):
-    y = y[np.logical_and(np.logical_and(X[:, i] >= -1, X[:, i] <= 9), X[:, i] != 0)]
-    X = X[np.logical_and(np.logical_and(X[:, i] >= -1, X[:, i] <= 9), X[:, i] != 0)]
-"""
 
-# Onehotting categorical features
-onehot = sklpre.OneHotEncoder(categories="auto")
-X = sklco.ColumnTransformer(
-    [("", onehot, [1, 2, 3])], remainder="passthrough").fit_transform(X)
+X_trim = pd.DataFrame(X, columns=features.columns.values)
 
-X = np.append(np.ones_like(X[:, 1]).reshape(-1, 1), X, axis=1)
+col_encode = list(X_trim.columns.values[1:4])
+col_scale = [X_trim.columns.values[0]] + list(X_trim.columns.values[4:])
 
-# Export X and y as numpy arrays
-np.save("data/design_matrix_credit.npy", X)
+onehot = sklpre.OneHotEncoder(categories="auto", sparse=False)
+scaler = sklpre.StandardScaler()
+
+encoded_cols = onehot.fit_transform(X_trim[col_encode])
+scaled_cols = scaler.fit_transform(X_trim[col_scale])
+
+X_trimmed = np.concatenate([encoded_cols, scaled_cols], axis=1)
+
+# Add on intercept column
+X_final = np.append(np.ones_like(X_trimmed[:, 1]).reshape(-1, 1), X_trimmed, axis=1)
+
+np.save("data/design_matrix_credit.npy", X_final)
 np.save("data/targets_credit.npy", y)
