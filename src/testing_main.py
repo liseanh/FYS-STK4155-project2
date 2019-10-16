46af4643ebd4ebad8d05498cc879f5f6daa880b2
Lise Nguyen <lise_nguyen@outlook.com>
2019-10-16 14:39:07 +0200

    found some parameters that don't underfit as much anymore

--- src/testing_main.py
+++ src/testing_main.py
@@ -9,7 +9,7 @@ y = np.load("data/targets_credit.npy")
 X_train, X_test, y_train, y_test = sklms.train_test_split(X, y, test_size=0.33, stratify=y)
 
 
-test = NeuralNetwork(n_epochs=1000, learning_rate=1e-5)
+test = NeuralNetwork(n_epochs=200, learning_rate=1e-5, hidden_layer_size=(500,400,300,100))
 test.fit(X_train, y_train)
 print(test.predict(X_train), y_train)
 print(test.accuracy_score(X_train, y_train))
