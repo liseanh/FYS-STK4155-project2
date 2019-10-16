bfc4b77a262e1077e1b32759392c753d8d8bd9c6
bernharl <bernharl@student.matnat.uio.no>
2019-10-16 09:36:55 +0200

    Created backpropagation, hopefully it works. Need to test it

--- src/main.py
+++ src/main.py
@@ -27,23 +27,6 @@ class RegressionClass:
     def fit(self, X=None, y=None):
         raise RuntimeError("Please do not use this class directly.")
 
-    def gradient_descent(self, beta, X, y):
-        n_iterations = len(y) // self.batch_size(len(y))
-        y_batches = np.array_split(y, n_iterations)
-        X_batches = np.array_split(X, n_iterations, axis=0)
-        for i in range(self.n_epochs):
-            for j in range(n_iterations):
-                random_batch = np.random.randint(n_iterations)
-                grad = self.learning_rate * self.grad_cost_function(
-                    beta, X_batches[random_batch], y_batches[random_batch]
-                )
-                rdiff = np.max(np.abs(grad / beta))
-                if rdiff < self.rtol:
-                    print("Tolerance reached")
-                    return
-
-                beta -= grad
-
     def accuracy_score(self, X, y):
         return np.mean(self.predict(X) == y)
 
@@ -60,6 +43,23 @@ class LogisticRegression(RegressionClass):
 
     def grad_cost_function(self, beta, X, y):
         return -X.T @ (y - self.p(beta, X))
+
+    def gradient_descent(self, beta, X, y):
+        n_iterations = len(y) // self.batch_size(len(y))
+        y_batches = np.array_split(y, n_iterations)
+        X_batches = np.array_split(X, n_iterations, axis=0)
+        for i in range(self.n_epochs):
+            for j in range(n_iterations):
+                random_batch = np.random.randint(n_iterations)
+                grad = self.learning_rate * self.grad_cost_function(
+                    beta, X_batches[random_batch], y_batches[random_batch]
+                )
+                rdiff = np.max(np.abs(grad / beta))
+                if rdiff < self.rtol:
+                    print("Tolerance reached")
+                    return
+
+                beta -= grad
 
     def predict(self, X):
         prediction = X @ self.beta
@@ -132,21 +132,60 @@ class NeuralNetwork(RegressionClass):
         return expo / (1 + expo)
 
     def feed_forward(self, X):
-        step = [X]  # self.activation(X)
+        a_i = [X]  # self.activation(X)
         for i in range(self.n_hidden_layers):
-            step.append(
-                self.sigmoid(step[i] @ self.weights_hidden[i] + self.biases_hidden[i])
+            a_i.append(
+                self.sigmoid(a_i[i] @ self.weights_hidden[i] + self.biases_hidden[i])
             )
-        step.append(self.sigmoid(step[-1] @ self.weights_out + self.biases_out))
-        print(step[-1].shape)
-        return step
+        a_i.append(self.sigmoid(a_i[-1] @ self.weights_out + self.biases_out))
+        # a_i.pop(0)
+        return a_i
 
     def grad_cost_function(self, model, target):
         return (model - target) / (model * (1 - model))
 
     def backpropagation(self, X, y):
-        forward_steps = self.feed_forward(X)
-        return gradients
+        a_i = self.feed_forward(X)
+        errors = [(y.reshape(-1, 1) - a_i[-1])]
+        gradients_weight = [a_i[-2].T @ errors[0]]
+        gradients_bias = [np.sum(errors[0], axis=0)]
+        print(gradients_bias[0].shape)
+        errors.append(errors[0] @ self.weights_out.T * a_i[-2] * (1 - a_i[-1]))
+        gradients_weight.append(a_i[-3].T @ errors[1])
+        gradients_bias.append(np.sum(errors[1], axis=0))
+        print(gradients_bias[1].shape)
+        for i in range(2, self.n_hidden_layers + 1):
+            errors.append(
+                errors[i - 1]
+                @ self.weights_hidden[-i + 1].T
+                * a_i[-i - 1]
+                * (1 - a_i[-i - 1])
+            )
+            gradients_weight.append(a_i[i - 1].T @ errors[i])
+            gradients_bias.append(np.sum(errors[i], axis=0))
+            print(gradients_bias[i].shape)
+        # exit()
+        return gradients_weight, gradients_bias
+
+    def gradient_descent(self, X, y):
+        """
+        Needs major rework for neural network!
+        """
+        n_iterations = len(y) // self.batch_size(len(y))
+        y_batches = np.array_split(y, n_iterations)
+        X_batches = np.array_split(X, n_iterations, axis=0)
+        for i in range(self.n_epochs):
+            for j in range(n_iterations):
+                random_batch = np.random.randint(n_iterations)
+                grads = self.learning_rate * self.backpropagation(
+                    X_batches[random_batch], y_batches[random_batch]
+                )
+                rdiff = np.max(np.abs(grads[-1] / beta[-1]))
+                if rdiff < self.rtol:
+                    print("Tolerance reached")
+                    return
+
+                beta -= grads
 
 
 if __name__ == "__main__":
