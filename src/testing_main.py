c46452b00525ba89861d680e82deb1cafe33838c
Lise Nguyen <lise_nguyen@outlook.com>
2019-10-22 16:54:23 +0200

    added scikit-learn's mlpclassifier using adam to compare, turns out sgd just sucks

--- src/testing_main.py
+++ src/testing_main.py
@@ -15,8 +15,8 @@ rate = 1e-5
 M = 200
 n = 200
 
-layer_size = (18, 12, 6)
-
+layer_size = (100, 50, 20)
+"""
 test = NeuralNetwork(
     n_epochs=n, batch_size=M, learning_rate=rate, hidden_layer_size=layer_size
 )
@@ -26,7 +26,7 @@ test.predict(X_train)
 print(test.accuracy_score(X_train, y_train))
 exit()
 # print(layer_size)
-
+"""
 reg = sknn.MLPClassifier(
     hidden_layer_sizes=layer_size,
     activation="logistic",
@@ -35,14 +35,27 @@ reg = sknn.MLPClassifier(
     max_iter=n,
     solver="sgd",
     batch_size=M,
-    tol=0,
     alpha=0,
     validation_fraction=0,
     momentum=0,
-    nesterovs_momentum=False,
+    tol=0,
     shuffle=False,
+    verbose=True,
 )
 
 reg = reg.fit(X_train, y_train)
 pred = reg.predict(X_test)
 print(reg.score(X_test, y_test), pred)
+"""
+reg_test = sknn.MLPClassifier(
+    hidden_layer_sizes=layer_size,
+    activation="relu",
+    max_iter=10000, 
+    verbose=True,
+
+)
+reg_test = reg_test.fit(X_train, y_train)
+pred = reg_test.predict(X_test)
+print(reg_test.score(X_test, y_test))
+print(reg_test.score(X_train, y_train))
+"""
