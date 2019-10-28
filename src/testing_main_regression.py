82cf5b12046a4e536c28843cf43b6c99d443ed3e
Lise Nguyen <lise_nguyen@outlook.com>
2019-10-28 07:52:28 +0100

    for running main.py on regression data

---
+++ src/testing_main_regression.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+def generate_Franke_data(x_points, y_points):
+    """
+    Generates data using Franke's function. 
+    """
+    x = np.linspace(0, 1, x_points, endpoint=True)
+    y = np.linspace(0, 1, y_points, endpoint=True)
+    
+    term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
+    term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
+    term3 = 0.5 * np.exp(-(9 * x - 7) ** 2 / 4.0 - 0.25 * ((9 * y - 3) ** 2))
+    term4 = -0.2 * np.exp(-(9 * x - 4) ** 2 - (9 * y - 7) ** 2)
+    return term1 + term2 + term3 + term4
+
