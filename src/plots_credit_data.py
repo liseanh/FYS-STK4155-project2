55192458eea44d7403b7c6ac1ada8760ebb8e9dc
bernharl <bernharl@student.matnat.uio.no>
2019-10-31 08:36:13 +0100

    Added theoretical best curve

--- src/plots_credit_data.py
+++ src/plots_credit_data.py
@@ -21,6 +21,22 @@ proba_split = np.append(proba_0, proba_1, axis=1)
 # proba_and_ideal = np.append(y_test, y_pred, axis=1)
 # print(proba_and_ideal.shape)
 
+def bestCurve(y):
+	defaults = np.sum(y == 1, dtype=np.int)
+	total = len(y)
+	x = np.linspace(0, 1, total)
+	y1 = np.linspace(0, 1, defaults)
+	y2 = np.ones(total-defaults)
+	y3 = np.concatenate([y1,y2])
+	return x, y3
+
+x, y_arrays = bestCurve(y_test)
+print(x, y_arrays)
+
 skplt.metrics.plot_cumulative_gain(y_test.ravel(), proba_split)
-plt.legend(["Not default", "Default"])
+plt.plot(x, y_arrays)
+#skplt.metrics.plot_cumulative_gain(y_test.ravel(), np.random.choice([0, 1], size=[len(y_test.ravel()), 2]))
+#skplt.metrics.plot_cumulative_gain(y_test.ravel(), proba_split)
+plt.legend(["Not default", "Default", "Baseline", "Perfect model"])
+plt.axis([x[0], x[-1], 0, 1.01])
 plt.show()
