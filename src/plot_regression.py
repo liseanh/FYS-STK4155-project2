180dd664def479ccbc6c4af1f46b8ef7f25e10e1
Lise Nguyen <lise_nguyen@outlook.com>
2019-11-02 21:48:19 +0100

    loads franke model and makes plot

---
+++ src/plot_regression.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from main import MultilayerPerceptronRegressor
+import sklearn.preprocessing as sklpre
+import sklearn.model_selection as sklms
+from mpl_toolkits.mplot3d import Axes3D
+
+training_set = np.load("data/franke_data_train.npz")
+test_set = np.load("data/franke_data_test.npz")
+meshgrid = np.load("data/franke_data_meshgrid.npz")
+
+x_meshgrid, y_meshgrid, z_meshgrid = meshgrid["x"], meshgrid["y"], meshgrid["z"]
+
+
+X_train, z_train = training_set["X_train"], training_set["z_train"]
+X_test, z_test = test_set["X_test"], test_set["z_test"]
+
+
+model = MultilayerPerceptronRegressor() 
+model.load_model("reg_test.npz")
+
+
+y_pred_train = model.predict(X_train)
+y_pred_test = model.predict(X_test)
+
+
+scaler = sklpre.StandardScaler().fit(X_train)
+
+scaler_output = sklpre.MinMaxScaler().fit(z_train)
+y_pred_train = scaler_output.inverse_transform(y_pred_train)
+y_pred_test = scaler_output.inverse_transform(y_pred_test)
+
+
+fig = plt.figure()
+fig.set_size_inches(3.03, 1.8)
+ax = fig.gca(projection="3d")
+
+
+surf = ax.plot_surface(
+    x_meshgrid,
+    y_meshgrid,
+    z_meshgrid,
+    cmap=matplotlib.cm.coolwarm,
+    linewidth=0,
+    antialiased=False,
+    alpha=0.2,
+)
+
+
+ax.scatter(
+    scaler.inverse_transform(X_train)[:, 0],
+    scaler.inverse_transform(X_train)[:, 1],
+    y_pred_train,
+    marker=".",
+    label="train",
+)
+ax.scatter(
+    scaler.inverse_transform(X_test)[:, 0],
+    scaler.inverse_transform(X_test)[:, 1],
+    y_pred_test,
+    marker=".",
+    label="test",
+)
+# ax.legend()
+ax.axis("off")
+ax.grid(False)
+ax.set_frame_on(False)
+plt.savefig(
+    "../doc/figures/regtest.pdf", bbox_inches="tight", pad_inches=0, dpi=1000
+)
+# plt.show()
