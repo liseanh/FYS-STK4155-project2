01d4e86e9c05e842f8c952fd153e5cb933360721
bernharl <bernharl@student.matnat.uio.no>
2019-11-03 13:52:47 +0100

    Added test function for classification

--- src/test_main.py
+++ src/test_main.py
@@ -1,26 +1,43 @@
-import numpy as np 
+import numpy as np
 from main import MultilayerPerceptronClassifier, MultilayerPerceptronRegressor
 import sklearn.preprocessing as sklpre
 import matplotlib.pyplot as plt
 
-def test_MultilayerPerceptronRegressor():
+np.random.seed(len("jeff"))
+
+
+def test_MultilayerPerceptronRegressor_overfit_simple():
     x = np.linspace(0, 5, 10000, endpoint=True)
-    y = 3*x + 2
-    x = x.reshape(-1,1)
-    y = y.reshape(-1,1)
-    print(x.shape)
+    y = 3 * x
+    x = x.reshape(-1, 1)
+    y = y.reshape(-1, 1)
     scalerx = sklpre.StandardScaler().fit(x)
     x = scalerx.transform(x)
-
-    scalery = sklpre.MinMaxScaler().fit(y)
-    y = scalery.transform(y)
-
-    model = MultilayerPerceptronRegressor(hidden_layer_size=[1], rtol=-np.inf, learning_rate=1e-3)
+    model = MultilayerPerceptronRegressor(
+        n_epochs=100,
+        hidden_layer_size=[3],
+        rtol=-np.inf,
+        learning_rate=2e-3,
+        verbose=False,
+    )
     model.fit(x, y)
     y_model = model.predict(x)
-    plt.plot(x, y, x, y_model)
-    plt.show()
-    #print(y_model-y)
-    #np.testing.assert_array_almost_equal(y, y_model, decimal=3)
+    np.testing.assert_array_almost_equal(y, y_model, decimal=1)
 
-test_MultilayerPerceptronRegressor()
+
+def test_MultilayerPerceptronClassifier_overfit_simple():
+    y = np.zeros(1000)
+    X = np.random.randint(0, 2, size=y.shape)
+    y[X == 1] = 1
+    X = X.reshape(-1, 1)
+    y = y.reshape(-1, 1)
+    model = MultilayerPerceptronClassifier(
+        n_epochs=50,
+        batch_size=32,
+        hidden_layer_size=[10],
+        learning_rate=1,
+        verbose=False,
+    )
+    model.fit(X, y)
+    y_model = model.predict(X)
+    np.testing.assert_array_equal(y, y_model)
