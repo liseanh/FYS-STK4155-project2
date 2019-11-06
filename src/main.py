083146ae7e7e6cb82f3c93f0dfee39cbfd9b0f43
bernharl <bernharl@student.matnat.uio.no>
2019-11-06 15:55:06 +0100

    Syntax error

--- src/main.py
+++ src/main.py
@@ -17,39 +17,39 @@ class RegressionClass(sklbase.BaseEstimator, sklbase.ClassifierMixin):
         verbose=False,
         learning_schedule=None,
     ):
-    """
-    Base class for the logistic regressor and the multilayer perceptron regressor 
-    and classifier. Please do not call this class on its own as it does not 
-    contain any regression methods by itself.
+        """
+        Base class for the logistic regressor and the multilayer perceptron regressor
+        and classifier. Please do not call this class on its own as it does not
+        contain any regression methods by itself.
 
-    Parameters: 
+        Parameters:
 
-    learning rate: float, default 0.1 
-        The learning rate used in the stochastic gradient descent solver.
+        learning rate: float, default 0.1
+            The learning rate used in the stochastic gradient descent solver.
 
-    n_epochs: int, default 2000
-        number of epochs used in the stochastic gradient descent solver.
+        n_epochs: int, default 2000
+            number of epochs used in the stochastic gradient descent solver.
 
-    rtol: float, default 0.01
-        Relative tolerance used as a stopping criteria in the stochastic gradient
-        descent solver. 
+        rtol: float, default 0.01
+            Relative tolerance used as a stopping criteria in the stochastic gradient
+            descent solver.
 
-    batch_size: int, default "auto"
-        Size of the minibatches used in the stochastic gradient descent solver.
-        If "auto", then batch_size = min(200, N), where N is the number of 
-        input samples.
-    
-    penalty: float, default None
-        The L2 shrinkage parameter used for regularisation of the weights in the 
-        MultilayerPerceptronClassifier and MultilayerPerceptronRegressor.
+        batch_size: int, default "auto"
+            Size of the minibatches used in the stochastic gradient descent solver.
+            If "auto", then batch_size = min(200, N), where N is the number of
+            input samples.
 
-    verbose: bool, default False
-        Whether or not to print progress in terminal 
+        penalty: float, default None
+            The L2 shrinkage parameter used for regularisation of the weights in the
+            MultilayerPerceptronClassifier and MultilayerPerceptronRegressor.
 
-    learning_schedule:  float, default None
-        deprecated
+        verbose: bool, default False
+            Whether or not to print progress in terminal
 
-    """
+        learning_schedule:  float, default None
+            deprecated
+
+        """
         self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.n_epochs = n_epochs
@@ -60,12 +60,12 @@ class RegressionClass(sklbase.BaseEstimator, sklbase.ClassifierMixin):
 
     def get_batch_size(self, len_y):
         """
-        Returns the batch size of the minibatches. 
+        Returns the batch size of the minibatches.
 
-        Parameter: 
+        Parameter:
 
         len_y: int
-            Number of data samples 
+            Number of data samples
         """
         if self.batch_size == "auto":
             return np.min([200, len_y])
@@ -95,7 +95,7 @@ class RegressionClass(sklbase.BaseEstimator, sklbase.ClassifierMixin):
 
 class LogisticRegression(RegressionClass):
     """
-    Inherits RegressionClass. 
+    Inherits RegressionClass.
     Performs logistic regression for data set X and corresponding binary output y
     """
     def fit(self, X, y):
